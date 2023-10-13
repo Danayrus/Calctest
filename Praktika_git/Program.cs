72fdf5fa77d0902e3c34891a66c8e177a8dc05e9
Danayrus <akindinovadana@gmail.com>
2023-10-13 09:40:52 +0300

    Сложение

---
+++ Praktika_git/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int resultAdd;
+            Console.WriteLine("Введите первое число");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            resultAdd = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {resultAdd}");
+            Console.ReadKey();
+
+        }
+    }
+}
